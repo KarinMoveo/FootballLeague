e2334ec01e671e59f661dc8e08fdb806a6fb78d0
KarinMoveo <karino@moveo.co.il>
2023-08-15 14:39:02 +0300

    Implemented modular code structure with separate API handling for leagues and teams

---
+++ src/api.ts
@@ -0,0 +1,49 @@
+import axios from 'axios';
+
+// Define the API URL for all leagues
+const apiUrl = `https://www.thesportsdb.com/api/v1/json/${process.env.API_KEY}/all_leagues.php`;
+
+// Define the API URL for teams based on league name
+const teamsApiUrl = `https://www.thesportsdb.com/api/v1/json/${process.env.API_KEY}/search_all_teams.php?l=`;
+
+// Define the structure of the objects returned from the API
+interface League {
+  idLeague: string;
+  strLeague: string;
+  strSport: string;
+}
+
+interface Team {
+  strTeamBadge: string;  // Team logo
+  strTeam: string;       // Team name
+}
+
+// Fetche football leagues information from an API, filters and returns the first 5 American Football leagues
+export async function fetchFootballLeagues() {
+
+  try {
+
+    const response = await axios.get(apiUrl);
+    const allLeagues: League[] = response.data.leagues;
+
+    // Filter out football leagues
+    const footballLeagues = allLeagues.filter((league: League) => league.strSport === 'American Football');
+
+    return footballLeagues.slice(0, 5);
+  } catch (error) {
+    console.error('Error fetching data:', error);
+    return [];
+  }
+}
+
+// Fetche teams information for a given league name from an API, returning an array of teams
+export async function fetchTeamsForLeague(leagueName: string) {
+  try {
+    const teamsResponse = await axios.get(`${teamsApiUrl}${encodeURIComponent(leagueName)}`);
+    const leagueTeams: Team[] = teamsResponse.data.teams;
+    return leagueTeams;
+  } catch (error) {
+    console.error('Error fetching teams data:', error);
+    return [];
+  }
+}
--- src/index.ts
+++ src/index.ts
@@ -1,91 +1,56 @@
 import './style.scss';
-import axios from 'axios';
+import { fetchTeamsForLeague, fetchFootballLeagues } from './api';
 
-// Define the API URL for all leagues
-const apiUrl = `https://www.thesportsdb.com/api/v1/json/${process.env.API_KEY}/all_leagues.php`;
+// Get the tab container & teams container 
+const tabContainer = document.querySelector('.tab-container');
+const teamsContainer = document.querySelector('.teams-container');
 
-// Define the API URL for teams based on league name
-const teamsApiUrl = `https://www.thesportsdb.com/api/v1/json/${process.env.API_KEY}/search_all_teams.php?l=`;
+/*
+Populates tab buttons with the names of football leagues
+Upon clicking a tab button, dynamically fetches and displays the teams for the selected league in the DOM
+*/
+async function populateTabButtons() {
 
-// Define the structure of the objects returned from the API
-interface League {
-  idLeague: string;
-  strLeague: string;
-  strSport: string;
-}
-interface Team {
-  strTeamBadge: string;  // Team logo
-  strTeam: string;       // Team name
-}
+  const footballLeagues = await fetchFootballLeagues();
 
-// Fatch football leagues information from an API, creates tab buttons for selected leagues & populates the teams for the selected league in the DOM upon click
-async function fetchFootballLeagues() {
+  // Populate the tab buttons with league names
+  footballLeagues.forEach((league) => {
+    const tabButton = document.createElement('button');
+    tabButton.classList.add('tab-button');
+    tabButton.textContent = league.strLeague;
+    tabContainer.appendChild(tabButton);
 
-  try {
-    const response = await axios.get(apiUrl);
-    const allLeagues = response.data.leagues;
+     // Add click event listener to the tab button
+    tabButton.addEventListener('click', async () => {
+      const leagueTeams = await fetchTeamsForLeague(league.strLeague);
 
-    // Filter out football leagues
-    const footballLeagues = allLeagues.filter((league: League) => league.strSport === 'American Football');
+      // Clear previous team data
+      teamsContainer.innerHTML = '';
 
-    // Get the tab container
-    const tabContainer = document.querySelector('.tab-container');
+      // The user need to know which league he pressed
+      const whichLeagueSelected = document.createElement('span');
+      whichLeagueSelected.classList.add('which-league-selected-message');
+      whichLeagueSelected.textContent = `This is the teams in "${league.strLeague}":`;
+      teamsContainer.appendChild(whichLeagueSelected);
 
-    // Populate the tab buttons with league names
-    footballLeagues.slice(0, 5).forEach((league: League) => {
-      const tabButton = document.createElement('button');
-      tabButton.classList.add('tab-button');
-      tabButton.textContent = league.strLeague;
-      tabContainer.appendChild(tabButton);
+      // Create teams list for the selected league
+      leagueTeams.forEach((team) => {
+        const teamContainer = document.createElement('div');
+        teamContainer.classList.add('team');
 
-      // Add click event listener to the tab button
-      tabButton.addEventListener('click', async () => {
+        const teamLogo = document.createElement('img');
+        teamLogo.src = team.strTeamBadge;
+        teamLogo.alt = `${team.strTeam} Logo`;
 
-        try {
+        const teamName = document.createElement('span');
+        teamName.textContent = team.strTeam;
 
-          // Fetch teams for the selected league using league name
-          const teamsResponse = await axios.get(`${teamsApiUrl}${encodeURIComponent(league.strLeague)}`);
-          const leagueTeams: Team[] = teamsResponse.data.teams;
-          console.log(leagueTeams);
-              
-          // Get the teams container
-          const teamsContainer = document.querySelector('.teams-container');
-
-          // Clear previous team data
-          teamsContainer.innerHTML = '';
-
-          // The user need to know which league he pressed
-          const whichLeagueSelected = document.createElement('span');
-          whichLeagueSelected.classList.add('which-league-selected-message');
-          whichLeagueSelected.textContent = `This is the teams in "${league.strLeague}":`;
-          teamsContainer.appendChild(whichLeagueSelected);
-
-          // Create teams list for the selected league
-          leagueTeams.forEach((team: Team) => {
-            const teamContainer = document.createElement('div');
-            teamContainer.classList.add('team');
-
-            const teamLogo = document.createElement('img');
-            teamLogo.src = team.strTeamBadge;
-            teamLogo.alt = `${team.strTeam} Logo`;
-
-            const teamName = document.createElement('span');
-            teamName.textContent = team.strTeam;
-
-            teamContainer.appendChild(teamLogo);
-            teamContainer.appendChild(teamName);
-            teamsContainer.appendChild(teamContainer);
-          });
-
-        } catch (error) {
-          console.error('Error fetching teams data:', error);
-        }
+        teamContainer.appendChild(teamLogo);
+        teamContainer.appendChild(teamName);
+        teamsContainer.appendChild(teamContainer);
       });
     });
-    
-  } catch (error) {
-    console.error('Error fetching data:', error);
-  }
+  });
 }
 
-fetchFootballLeagues();
+populateTabButtons();
